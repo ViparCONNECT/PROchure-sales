58715d0a6c6903643b190d69c95bdf550d06c424
Sabari V R <mailtosabarivr@gmail.com>
2025-12-06 12:03:37 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# PROchure-sales
--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/prochure-sales/',
-  plugins: [react()],
-})
