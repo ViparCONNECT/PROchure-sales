fd1e802b8a3278c11e7ca32f025d9ab667240460
SABARI V R <114075783+sabari-vr@users.noreply.github.com>
2026-08-25 16:17:28 +0530

    Remove unused import of useLocation from Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
